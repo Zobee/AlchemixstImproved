1542c27a0ad8d3f62f6521e1eaff41df7fcc8794
Zobee <zohebboga@hotmail.com>
2021-07-21 20:11:50 -0400

    Changed sounds and updated pads to utilize new sounds

--- alchemixst/src/components/DrumPad/padData.js
+++ alchemixst/src/components/DrumPad/padData.js
@@ -2,99 +2,99 @@ const padData = [
     {
         id: 1, 
         keyInput: '1', 
-        beatName:'Open Hat', 
-        sound: 'sounds/openHH.wav'
+        beatName:'Ride 1', 
+        sound: 'sounds/ride.wav'
     },
     {
         id: 2, 
         keyInput: '2', 
-        beatName:'Closed Hat', 
-        sound: 'sounds/closedHH.wav'
+        beatName:'Percussion 1', 
+        sound: 'sounds/percussion.wav'
     },
     {
         id: 3, 
         keyInput: '3', 
-        beatName:'Ride', 
-        sound: 'sounds/ride.wav'
+        beatName:'Percussion 2', 
+        sound: 'sounds/percussion2.wav'
     },
     {
         id: 4, 
         keyInput: '4', 
-        beatName:'Snare', 
-        sound: 'sounds/snare1.wav'
+        beatName:'Ride 2', 
+        sound: 'sounds/ride2.wav'
     },
     {
         id: 5, 
         keyInput: 'Q', 
-        beatName:'Snare', 
-        sound: 'sounds/snare2.wav'
+        beatName:'Open HH 1', 
+        sound: 'sounds/openHH.wav'
     },
     {
         id: 6, 
         keyInput: 'W', 
-        beatName:'Clap', 
-        sound: 'sounds/clap.wav'
+        beatName:'Closed HH 1', 
+        sound: 'sounds/closedHH.wav'
     },
     {
         id: 7, 
         keyInput: 'E', 
-        beatName:'Kick', 
-        sound: 'sounds/kick1.wav'
+        beatName:'Closed HH 2', 
+        sound: 'sounds/closedHH2.wav'
     },
     {
         id: 8, 
         keyInput: 'R', 
-        beatName:'Kick', 
-        sound: 'sounds/kick2.wav'
+        beatName:'Open HH 2', 
+        sound: 'sounds/openHH2.wav'
     },
     {
         id: 9, 
         keyInput: 'A', 
-        beatName:'Crash', 
-        sound: 'sounds/cymbal.wav'
+        beatName:'Clap', 
+        sound: 'sounds/clap.wav'
     },
     {
         id: 10, 
         keyInput: 'S', 
-        beatName:'Crash', 
-        sound: 'sounds/cymbal.wav'
+        beatName:'Snare 1', 
+        sound: 'sounds/snare.wav'
     },
     {
         id: 11, 
         keyInput: 'D', 
-        beatName:'Crash', 
-        sound: 'sounds/cymbal.wav'
+        beatName:'Snare 2', 
+        sound: 'sounds/snare2.wav'
     },
     {
         id: 12, 
         keyInput: 'F', 
-        beatName:'Crash', 
-        sound: 'sounds/cymbal.wav'
+        beatName:'Rim Shot', 
+        sound: 'sounds/rimshot.wav'
     },
     {
         id: 13, 
         keyInput: 'Z', 
-        beatName:'Crash', 
+        beatName:'Cymbal', 
         sound: 'sounds/cymbal.wav'
     },
     {
         id: 14, 
         keyInput: 'X', 
-        beatName:'Crash', 
-        sound: 'sounds/cymbal.wav'
+        beatName:'Kick 1', 
+        sound: 'sounds/kick.wav'
     },
     {
         id: 15, 
         keyInput: 'C', 
-        beatName:'Crash', 
-        sound: 'sounds/cymbal.wav'
+        beatName:'Kick 2', 
+        sound: 'sounds/kick2.wav'
     },
     {
-        id: 16, 
-        keyInput: 'V', 
-        beatName:'Crash', 
-        sound: 'sounds/cymbal.wav'
-    },
+        id: 16,
+        keyInput: "V",
+        beatName: "Crash",
+        sound: "sounds/crash.wav"
+    }
 ]
 
 export default padData
