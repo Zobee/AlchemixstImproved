8c99d3dfe20cff7436ab6de6bc99dcbb67e23694
Zoheb Boga <zohebboga@hotmail.com>
2021-08-29 14:30:08 -0400

    Added styling for player container and tracklist

--- alchemixst/src/components/MediaPlayer/PlayerContainer.js
+++ alchemixst/src/components/MediaPlayer/PlayerContainer.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import CurrentlyPlaying from './CurrentlyPlaying'
 import TrackSelection from './TrackSelection'
 
 const PlayerContainer = () => {
   return (
     <section className='player-container'>
-      <CurrentlyPlaying />
       <TrackSelection />
     </section>
   )
